a35e7a92ba0a2144fab5d3db8f0b04c473877e36
TZTsai <13818704679@163.com>
2021-12-02 23:02:02 +0100

    clone finrl and seed-rl

---
+++ project/drl_agents/seed_rl/run_local.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+# Copyright 2019 The SEED Authors
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+set -e
+die () {
+    echo >&2 "$@"
+    exit 1
+}
+
+ENVIRONMENTS="atari|dmlab|football|mujoco"
+AGENTS="r2d2|vtrace|sac|ppo"
+[ "$#" -ne 0 ] || die "Usage: run_local.sh [$ENVIRONMENTS] [$AGENTS] [Num. actors]"
+echo $1 | grep -E -q $ENVIRONMENTS || die "Supported games: $ENVIRONMENTS"
+echo $2 | grep -E -q $AGENTS || die "Supported agents: $AGENTS"
+echo $3 | grep -E -q "^((0|([1-9][0-9]*))|(0x[0-9a-fA-F]+))$" || die "Number of actors should be a non-negative integer without leading zeros"
+export ENVIRONMENT=$1
+export AGENT=$2
+export NUM_ACTORS=$3
+shift 3
+if [[ $1 ]]; then
+  echo $1 | grep -E -q "^((0|([1-9][0-9]*))|(0x[0-9a-fA-F]+))$" || die "Number of environments per actor should be a non-negative integer without leading zeros"
+  export ENV_BATCH_SIZE=$1
+  shift 1
+else
+  export ENV_BATCH_SIZE=1
+fi
+export CONFIG=$ENVIRONMENT
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd $DIR
+docker/build.sh
+docker_version=$(docker version --format '{{.Server.Version}}')
+if [[ "19.03" > $docker_version ]]; then
+  docker run --entrypoint ./docker/run.sh -ti -it -p 6006-6015:6006-6015 --name seed --rm seed_rl:$ENVIRONMENT $ENVIRONMENT $AGENT $NUM_ACTORS $ENV_BATCH_SIZE $@
+else
+  docker run --gpus all --entrypoint ./docker/run.sh -ti -it -p 6006-6015:6006-6015 -e HOST_PERMS="$(id -u):$(id -g)" --name seed --rm seed_rl:$ENVIRONMENT $ENVIRONMENT $AGENT $NUM_ACTORS $ENV_BATCH_SIZE $@
+fi
