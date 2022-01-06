9bfdb773a351beea25f5651dbf8d67c624e241ba
TZTsai <13818704679@163.com>
2022-01-06 23:20:30 +0100

    start project

--- lab2/losses.py
+++ lab2/losses.py
@@ -1,105 +1,48 @@
 import torch
 from torch import nn, Tensor
 from typing import Iterable, Dict
-from sentence_transformers.util import cos_sim
-from sentence_transformers import SentenceTransformer
 
 
 class CosineSimilarityLoss(nn.Module):
     """
-    CosineSimilarityLoss expects, that the InputExamples consists of two texts and a float label.
-
-    It computes the vectors u = model(input_text[0]) and v = model(input_text[1]) and measures the cosine-similarity between the two.
-    By default, it minimizes the following loss: ||input_label - cos_score_transformation(cosine_sim(u,v))||_2.
-
-    :param model: SentenceTranformer model
-    :param loss_fct: Which pytorch loss function should be used to compare the cosine_similartiy(u,v) with the input_label? By default, MSE:  ||input_label - cosine_sim(u,v)||_2
-    :param cos_score_transformation: The cos_score_transformation function is applied on top of cosine_similarity. By default, the identify function is used (i.e. no change).
-
-    Example::
-
-            from sentence_transformers import SentenceTransformer, SentencesDataset, InputExample, losses
-
-            model = SentenceTransformer('distilbert-base-nli-mean-tokens')
-            train_examples = [InputExample(texts=['My first sentence', 'My second sentence'], label=0.8),
-                InputExample(texts=['Another pair', 'Unrelated sentence'], label=0.3)]
-            train_dataset = SentencesDataset(train_examples, model)
-            train_dataloader = DataLoader(train_dataset, shuffle=True, batch_size=train_batch_size)
-            train_loss = losses.CosineSimilarityLoss(model=model)
-
-
+    Cosine similarity loss used in STS regression.
     """
-
-    def __init__(self, model: SentenceTransformer, loss_fct=nn.MSELoss(), cos_score_transformation=nn.Identity()):
-        super(CosineSimilarityLoss, self).__init__()
+    
+    def __init__(self, model: nn.Module):
+        super().__init__()
         self.model = model
-        self.loss_fct = loss_fct
-        self.cos_score_transformation = cos_score_transformation
 
     def forward(self, sentence_features: Iterable[Dict[str, Tensor]], labels: Tensor):
         embeddings = [self.model(sentence_feature)['sentence_embedding']
                       for sentence_feature in sentence_features]
-        output = self.cos_score_transformation(
-            torch.cosine_similarity(embeddings[0], embeddings[1]))
-        return self.loss_fct(output, labels.view(-1))
+        emb1, emb2 = embeddings
+        sim = torch.cosine_similarity(emb1, emb2)
+        return (sim - labels.view(-1)) ** 2
 
 
-class MultipleNegativesRankingLoss(nn.Module):
+class SoftmaxLoss(nn.Module):
     """
-        This loss expects as input a batch consisting of sentence pairs (a_1, p_1), (a_2, p_2)..., (a_n, p_n)
-        where we assume that (a_i, p_i) are a positive pair and (a_i, p_j) for i!=j a negative pair.
-
-        For each a_i, it uses all other p_j as negative samples, i.e., for a_i, we have 1 positive example (p_i) and
-        n-1 negative examples (p_j). It then minimizes the negative log-likehood for softmax normalized scores.
-
-        This loss function works great to train embeddings for retrieval setups where you have positive pairs (e.g. (query, relevant_doc))
-        as it will sample in each batch n-1 negative docs randomly.
-
-        The performance usually increases with increasing batch sizes.
-
-        For more information, see: https://arxiv.org/pdf/1705.00652.pdf
-        (Efficient Natural Language Response Suggestion for Smart Reply, Section 4.4)
-
-        You can also provide one or multiple hard negatives per anchor-positive pair by structering the data like this:
-        (a_1, p_1, n_1), (a_2, p_2, n_2)
-
-        Here, n_1 is a hard negative for (a_1, p_1). The loss will use for the pair (a_i, p_i) all p_j (j!=i) and all n_j as negatives.
-
-        Example::
-
-            from sentence_transformers import SentenceTransformer, losses, InputExample
-            from torch.utils.data import DataLoader
-
-            model = SentenceTransformer('distilbert-base-uncased')
-            train_examples = [InputExample(texts=['Anchor 1', 'Positive 1']),
-                InputExample(texts=['Anchor 2', 'Positive 2'])]
-            train_dataloader = DataLoader(train_examples, shuffle=True, batch_size=32)
-            train_loss = losses.MultipleNegativesRankingLoss(model=model)
+    Softmax loss used in NLI classification.
     """
 
-    def __init__(self, model: SentenceTransformer, scale: float = 20.0, similarity_fct=cos_sim):
-        """
-        :param model: SentenceTransformer model
-        :param scale: Output of similarity function is multiplied by scale value
-        :param similarity_fct: similarity function between sentence embeddings. By default, cos_sim. Can also be set to dot product (and then set scale to 1)
-        """
-        super(MultipleNegativesRankingLoss, self).__init__()
+    def __init__(self,
+                 model: nn.Module,
+                 sentence_embedding_dimension: int,
+                 num_labels: int):
+        super().__init__()
         self.model = model
-        self.scale = scale
-        self.similarity_fct = similarity_fct
-        self.cross_entropy_loss = nn.CrossEntropyLoss()
+        self.num_labels = num_labels
+        self.classifier = nn.Linear(3 * sentence_embedding_dimension, num_labels)
 
     def forward(self, sentence_features: Iterable[Dict[str, Tensor]], labels: Tensor):
         reps = [self.model(sentence_feature)['sentence_embedding']
                 for sentence_feature in sentence_features]
-        embeddings_a = reps[0]
-        embeddings_b = torch.cat(reps[1:])
+        rep_a, rep_b = reps
+        
+        features = torch.cat(
+            [rep_a, rep_b, torch.abs(rep_a - rep_b)],
+            dim = 1)
 
-        scores = self.similarity_fct(embeddings_a, embeddings_b) * self.scale
-        # Example a[i] should match with b[i]
-        labels = torch.tensor(range(len(scores)),
-                              dtype=torch.long, device=scores.device)
-        return self.cross_entropy_loss(scores, labels)
-
-    def get_config_dict(self):
-        return {'scale': self.scale, 'similarity_fct': self.similarity_fct.__name__}
+        return nn.functional.cross_entropy(
+            self.classifier(features),
+            labels.view(-1))
--- lab2/main.py
+++ lab2/main.py
@@ -6,23 +6,27 @@ import argparse
 import IPython
 
 parser = argparse.ArgumentParser()
-parser.add_argument('type', type=str, choices=['clf', 'reg'])
-parser.add_argument('--epochs', type=int, default=4)
-parser.add_argument('--batch_size', type=int, default=16)
+parser.add_argument('type', type=str, choices=['sts', 'nli'])
+parser.add_argument('-ep', '--epochs', type=int, default=4)
+parser.add_argument('-bs', '--batch_size', type=int, default=16)
 parser.add_argument('--max_seq_length', type=int, default=None)
 parser.add_argument('--save_path', type=str, default=None)
 
 if IPython.get_ipython():
-    sys.argv = [..., 'reg']
+    sys.argv = [__file__, 'reg']
 args = parser.parse_args()
 
 if args.save_path is None:
     args.save_path = os.path.join('output', 'sbert_' + args.type)
+    
+logging.basicConfig(format='%(asctime)s - %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S',
+                    level=logging.INFO)
 
 # %% Load data
 from torch.utils.data import DataLoader
 
-if args.type == 'clf':
+if args.type == 'nli':
     dataset = data.load_nli()
 else:
     dataset = data.load_sts()
@@ -32,6 +36,8 @@ train_samples, dev_samples, test_samples = [
 
 train_dataloader = DataLoader(train_samples, shuffle=True, batch_size=args.batch_size)
 
+logging.info("Train: %d samples (%d batchs), dev: %d samples, test: %d samples", len(train_samples), len(train_dataloader), len(dev_samples), len(test_samples))
+
 # %% Build model
 import losses
 from sbert import SBert
@@ -39,12 +45,11 @@ from sentence_transformers.evaluation import EmbeddingSimilarityEvaluator
 
 model = SBert()
 
-if args.type == 'clf':
-    train_loss = losses.MultipleNegativesRankingLoss(model=model)
+if args.type == 'nli':
+    train_loss = losses.SoftmaxLoss(model=model)
 else:
     train_loss = losses.CosineSimilarityLoss(model=model)
 
-logging.info("Read STSbenchmark dev dataset")
 evaluator = EmbeddingSimilarityEvaluator.from_input_examples(dev_samples, name='dev')
 
 # %% Train the model
