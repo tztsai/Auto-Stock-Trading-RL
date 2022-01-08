fc14e9ce1aabaca133867cb263b6e7a4a252f783
TZTsai <13818704679@163.com>
2022-01-08 15:27:36 +0100

    update scalable agent

---
+++ project/drl_agents/scalable/models.py
@@ -0,0 +1,131 @@
+import gym
+import torch
+import numpy as np
+import tensorflow as tf
+
+from absl import app
+from absl import flags
+
+from .seed_rl.common import actor
+from .seed_rl.agents.vtrace import learner as VTraceLearner
+from .seed_rl.agents.vtrace.networks import ImpalaDeep as VTraceModel
+from .seed_rl.agents.sac import learner as SACLearner
+from .seed_rl.agents.sac.networks import ActorCriticLSTM as SACModel
+# from .impala.environments import FlowEnvironment
+
+
+MODELS = dict(
+    vtrace=VTraceModel,
+    # r2d2=...,
+    sac=SACModel,
+    # ppo=...
+)
+
+LEARNERS = dict(
+    vtrace=VTraceLearner,
+    # r2d2=...,
+    sac=SACLearner,
+    # ppo=...
+)
+
+FLAGS = flags.FLAGS
+
+# Optimizer settings.
+flags.DEFINE_float('learning_rate', 0.00048, 'Learning rate.')
+flags.DEFINE_float('adam_epsilon', 3.125e-7, 'Adam epsilon.')
+
+
+class DRLAgent:
+    """Implementations for DRL algorithms
+
+    Attributes
+    ----------
+        env: gym environment class
+            user-defined class
+        price_array: numpy array
+            OHLC data
+        tech_array: numpy array
+            techical data
+        turbulence_array: numpy array
+            turbulence/risk data
+    Methods
+    -------
+        get_model()
+            setup DRL algorithms
+        train_model()
+            train DRL algorithms in a train dataset
+            and output the trained model
+        DRL_prediction()
+            make a prediction in a test dataset and get results
+    """
+
+    def __init__(self, env: gym.Env, price_array, tech_array, turbulence_array):
+        self.env = env
+        self.price_array = price_array
+        self.tech_array = tech_array
+        self.turbulence_array = turbulence_array
+
+    def get_model(self, model_name, model_kwargs=None):
+        if model_name not in MODELS:
+            raise NotImplementedError("NotImplementedError")
+
+        self.learner = LEARNERS[model_name]
+        self.model_cls = MODELS[model_name]
+        self.model_kwargs = model_kwargs or {}
+
+        return self.create_agent()
+
+    def create_agent(self, action_space, obs_space, parametric_action_distribution):
+        if self.model_cls is VTraceModel:
+            self.model_kwargs['num_actions'] = action_space.n
+        return self.model_cls(**self.model_kwargs)
+
+    def create_env(self, *args):
+        return self.env
+
+    def create_optimizer(self, final_iteration):
+        # learning_rate_fn = tf.keras.optimizers.schedules.PolynomialDecay(
+        #     FLAGS.learning_rate, final_iteration, 0)
+        # optimizer = tf.keras.optimizers.Adam(learning_rate_fn, beta_1=0,
+        #                                      epsilon=FLAGS.adam_epsilon)
+        learning_rate_fn = lambda iteration: FLAGS.learning_rate
+        optimizer = tf.keras.optimizers.Adam(FLAGS.learning_rate)
+        return optimizer, learning_rate_fn
+
+    def train_model(self, model, total_env_frames=int(1e9)):
+        FLAGS.total_env_frames = total_env_frames
+        self.learner.learner_loop(
+            self.create_env,
+            self.create_agent,
+            self.create_optimizer
+        )
+
+    @staticmethod
+    def DRL_prediction(model_path, environment):
+        # load agent
+        try:
+            model = tf.saved_model.load(model_path)
+        except BaseException:
+            raise ValueError("Fail to load agent!")
+
+        # test on the testing env
+        state = environment.reset()
+        episode_returns = list()  # the cumulative_return / initial_account
+        episode_total_assets = list()
+        episode_total_assets.append(environment.initial_total_asset)
+        done = False
+        while not done:
+            action = model.predict(state)[0]  # FIXME
+            state, reward, done, _ = environment.step(action)
+
+            total_asset = (
+                environment.amount
+                + (environment.price_ary[environment.day] * environment.stocks).sum()
+            )
+            episode_total_assets.append(total_asset)
+            episode_return = total_asset / environment.initial_total_asset
+            episode_returns.append(episode_return)
+
+        print("episode_return", episode_return)
+        print("Test Finished!")
+        return episode_total_assets
